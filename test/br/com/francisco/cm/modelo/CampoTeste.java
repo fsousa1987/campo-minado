a23a0cb6bc9cec00a6ce4b72b523281e31d3d114
Francisco Sousa <franciscoeds1987@gmail.com>
2020-07-18 08:12:17 -0300

    Lógica e teste de adicionar vizinhos implementadas

---
+++ test/br/com/francisco/cm/modelo/CampoTeste.java
@@ -0,0 +1,58 @@
+package br.com.francisco.cm.modelo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CampoTeste {
+
+    private Campo campo;
+
+    @Before
+    public void iniciarCampo() {
+        campo = new Campo(3, 3);
+    }
+
+    @Test
+    public void testeVizinhoDistancia1Esquerda() {
+        Campo vizinho = new Campo(3, 2);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+
+    @Test
+    public void testeVizinhoDistancia1Direita() {
+        Campo vizinho = new Campo(3, 4);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+
+    @Test
+    public void testeVizinhoDistancia1EmCima() {
+        Campo vizinho = new Campo(2, 3);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+
+    @Test
+    public void testeVizinhoDistancia1Embaixo() {
+        Campo vizinho = new Campo(4, 3);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+
+    @Test
+    public void testeVizinhoDistancia2() {
+        Campo vizinho = new Campo(2, 2);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertTrue(resultado);
+    }
+
+    @Test
+    public void testeNaoVizinho() {
+        Campo vizinho = new Campo(1, 1);
+        boolean resultado = campo.adicionarVizinho(vizinho);
+        assertFalse(resultado);
+    }
+}
