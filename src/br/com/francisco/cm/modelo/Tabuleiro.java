1d1b4c06d3fc473e132b187d456047d1603f68bc
Francisco Sousa <franciscoeds1987@gmail.com>
2020-07-27 10:19:54 -0300

    Implementado código para exibir todos os campos após o jogador perder o jogo

--- src/br/com/francisco/cm/modelo/Tabuleiro.java
+++ src/br/com/francisco/cm/modelo/Tabuleiro.java
@@ -1,5 +1,7 @@
 package br.com.francisco.cm.modelo;
 
+import br.com.francisco.cm.excecao.ExplosaoException;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
@@ -23,10 +25,15 @@ public class Tabuleiro {
     }
 
     public void abrir(int linha, int coluna) {
-        campos.parallelStream()
-                .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
-                .findFirst()
-                .ifPresent(c -> c.abrir());
+        try {
+            campos.parallelStream()
+                    .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                    .findFirst()
+                    .ifPresent(c -> c.abrir());
+        } catch (ExplosaoException e) {
+            campos.forEach(c -> c.setAberto(true));
+            throw e;
+        }
     }
 
     public void alterarMarcacao(int linha, int coluna) {
