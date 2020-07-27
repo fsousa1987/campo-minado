42863dbac21951ebf7b160b3c2b492717e46088d
Francisco Sousa <franciscoeds1987@gmail.com>
2020-07-27 09:19:00 -0300

    Lógica de ciclo do jogo adicionada ao TabuleiroConsole

--- src/br/com/francisco/cm/Aplicacao.java
+++ src/br/com/francisco/cm/Aplicacao.java
@@ -1,17 +1,13 @@
 package br.com.francisco.cm;
 
 import br.com.francisco.cm.modelo.Tabuleiro;
+import br.com.francisco.cm.visao.TabuleiroConsole;
 
 public class Aplicacao {
 
     public static void main(String[] args) {
 
         Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
-
-        tabuleiro.abrir(3, 3);
-        tabuleiro.alterarMarcacao(4, 4);
-        tabuleiro.alterarMarcacao(4, 5);
-
-        System.out.println(tabuleiro);
+        new TabuleiroConsole(tabuleiro);
     }
 }
